e4a3b1246368919291e646d8718da40e15667890
chunyue <yueman82@gmail.com>
2024-06-23 15:24:42 +0800

    引入RouteLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
 
 export const routes: Routes = [
   {
     path: '',
+    title: 'App Home Page',
     component: HomeComponent,
+  },
+  {
+    path: 'create-product',
+    title: 'Create Product',
+    component: CreateProductComponent,
   },
 ];
