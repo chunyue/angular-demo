eea308194c7ee64e058367e725765018dc198199
chunyue <yueman82@gmail.com>
2024-06-24 00:25:40 +0800

    feat(Pagination): 分頁功能 1. 利用@if判斷

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,15 +14,54 @@ import { CommonModule } from '@angular/common';
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
+  //分頁所需參數
+  currentPage: number = 1;
+  totalPages: number = 1;
+  pageOffset: number = 0;
+  pageLimit: number = 5;
+  totalProducts: number = 0;
+
   products: Product[] = [];
   ngOnInit() {
-    this.productsService
-      .getProducts(`http://localhost:8080/products`, {})
-      .subscribe((ProductsReponse: ProductsReponse) => {
-        console.log(ProductsReponse);
-        this.products = ProductsReponse.results;
-      });
-
+    this.fetchProducts();
     console.log(this.products);
+  }
+  fetchProducts(): void {
+    this.productsService
+      .getProducts(
+        `http://localhost:8080/products?offset=${this.pageOffset}`,
+        {}
+      )
+      .subscribe((productsResponse: ProductsReponse) => {
+        console.log(productsResponse);
+        this.products = productsResponse.results;
+        this.totalProducts = Number(productsResponse.total);
+        this.pageLimit = Number(productsResponse.limit);
+        this.pageOffset = Number(productsResponse.offset);
+        this.currentPage =
+          Math.floor(
+            Number(productsResponse.offset) / Number(productsResponse.limit)
+          ) + 1;
+        this.totalPages = Math.ceil(
+          Number(productsResponse.total) / Number(productsResponse.limit)
+        );
+      });
+  }
+
+  //下一頁
+  nextPage(currentPage: number, pageLimit: number) {
+    this.pageOffset = currentPage * pageLimit;
+    this.fetchProducts();
+  }
+
+  //上一頁
+  previousPage(currentPage: number, pageLimit: number) {
+    if (currentPage > 2) {
+      this.pageOffset = (currentPage - 2) * pageLimit;
+      this.fetchProducts();
+    } else {
+      this.pageOffset = 0;
+      this.fetchProducts();
+    }
   }
 }
